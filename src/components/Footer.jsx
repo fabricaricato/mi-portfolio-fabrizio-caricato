6d2f5e6d00d40c57432d422a89c86adcdec2b9d8
Fabrizio <fabriziocaricato1@gmail.com>
2025-12-19 20:53:39 -0300

    Footer componetizado

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <>
+      <section>
+        <h2 id="contacts">Contacto</h2>
+        <p>LinkedIn: <a href="https://www.linkedin.com/in/fabrizio" target="_blank" rel="noopener noreferrer">linkedin.com/in/fabrizio</a></p>
+        <p>GitHub: <a href="https://github.com/tuusuario" target="_blank" rel="noopener noreferrer">github.com/tuusuario</a></p>
+      </section>
+      <footer>
+        &copy; 2025 Fabrizio Caricato. Todos los derechos reservados.
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
